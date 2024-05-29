207c9a0dba753036f938cfd1a31dade9567be92c
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-05-30 01:01:21 +0300

    entities created

---
+++ EntityLayer/Concrete/About.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class About
+    {
+        [Key]
+        public int AboutId { get; set; }
+        public string? Title { get; set; }
+        public string? Subtitle { get; set; }
+        public string? Description { get; set; }
+        public string? Name { get; set; }
+        public string? Life { get; set; }
+        public string? Mail { get; set; }
+        public string? Phone { get; set; }
+        public string? LinkedinUrl { get; set; }
+        public string? GithubUrl { get; set; }
+        public string? CvUrl { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactId { get; set; }
+        public string? Name { get; set; }
+        public string? Mail { get; set; }
+        public string? Phone { get; set; }
+        public string? MessageBody { get; set; }
+        public DateTime MessageDate { get; set; }
+        public bool MessageStatus { get; set; }
+    }
+}

70fa50896f2a985ab68012fb955940343953a2a8
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-06-01 02:31:45 +0300

    ErrorPage Page added to the project

---
+++ portfolio/Controllers/ErrorPageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        public IActionResult Error404(string code)
+        {
+            return View();
+        }
+    }
+}

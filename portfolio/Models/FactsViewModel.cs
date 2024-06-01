c8e44191224eed78883c1cef5d1818c01dcd546a
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-06-01 18:53:21 +0300

    facts section was made

---
+++ portfolio/Models/FactsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using EntityLayer.Concrete;
+
+namespace portfolio.Models
+{
+    public class FactsViewModel
+    {
+        public List<Skills> Skills { get; set; }
+        public List<Portfolio> Portfolio { get; set; }
+        public List<Services> Services { get; set; }
+    }
+}
--- portfolio/ViewComponents/Default/_Facts.cs
+++ portfolio/ViewComponents/Default/_Facts.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using portfolio.Models;
 
 namespace portfolio.ViewComponents.Default
 {
     public class _Facts : ViewComponent
     {
+        private readonly IPortfolioService _portfolioService;
+        private readonly IServicesService _servicesService;
+        private readonly ISkillsService _skillsService;
+
+        public _Facts(IPortfolioService portfolioService, IServicesService servicesService, ISkillsService skillsService)
+        {
+            _portfolioService = portfolioService;
+            _servicesService = servicesService;
+            _skillsService = skillsService;
+        }
+
         public IViewComponentResult Invoke()
         {
-            return View();
+            var portfolios = _portfolioService.TGetList();
+            var skills = _skillsService.TGetList();
+            var services = _servicesService.TGetList();
+
+            var model = new FactsViewModel()
+            {
+                Services = services,
+                Skills = skills,
+                Portfolio = portfolios,
+            };
+            return View(model);
         }
     }
 }

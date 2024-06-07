5a1c3eae8718d84d87b47bc928c8933666375978
cetinyazici <cetin.yazici30@erzurum.edu.tr>
2024-06-07 03:32:27 +0300

    program.cs edited

---
+++ BusinessLayer/Container/Extensions.cs
@@ -0,0 +1,81 @@
+﻿using BusinessLayer.Abstract;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules.Contact;
+using BusinessLayer.ValidationRules.Portfolios;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using DToLayer.ContactDtos;
+using EntityLayer.Concrete;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Container
+{
+    public static class Extensions
+    {
+        public static void ConfigureRepositoryRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IChooseusService, ChooseusManager>();
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IPortfolioService, PortfolioManager>();
+            services.AddScoped<IPortfolioDetailsService, PortfolioDetailsManager>();
+            services.AddScoped<IServicesService, ServicesManager>();
+            services.AddScoped<ISkillsService, SkillsManager>();
+            services.AddScoped<ISummaryService, SummaryManager>();
+            services.AddScoped<IEducationService, EducationManager>();
+            services.AddScoped<IExperienceService, ExperienceManager>();
+            services.AddScoped<ITestimonialService, TestimonialManager>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+        }
+        public static void ConfigureServiceRegistration(this IServiceCollection services)
+        {
+            services.AddScoped<IAboutDal, EfAboutDal>();
+            services.AddScoped<IChooseusDal, EfChooseusDal>();
+            services.AddScoped<IContactDal, EfContactDal>();
+            services.AddScoped<IPortfolioDal, EfPortfolioDal>();
+            services.AddScoped<IPortfolioDetailsDal, EfPortfolioDetailsDal>();
+            services.AddScoped<IServicesDal, EfServicesDal>();
+            services.AddScoped<ISkillsDal, EfSkillsDal>();
+            services.AddScoped<IExperienceDal, EfExperienceDal>();
+            services.AddScoped<ISummaryDal, EfSummaryDal>();
+            services.AddScoped<IEducationDal, EfEducationDal>();
+            services.AddScoped<ITestimonialDal, EfTestimonialDal>();
+            services.AddScoped<IAppUserDal, EfAppUserDal>();
+        }
+        public static void ConfigureApplicationCookie(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Account/SignIn/";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.SlidingExpiration = true;
+            });
+        }
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+            })
+            .AddEntityFrameworkStores<Context>()
+            .AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+        }
+        public static void ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddTransient<IValidator<SendMessageDto>, SendContactValiator>();
+            services.AddTransient<IValidator<Portfolio>, PortfolioValid>();
+        }
+
+    }
+}
--- portfolio/Program.cs
+++ portfolio/Program.cs
@@ -13,70 +13,32 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using portfolio.Models;
-
+using BusinessLayer.Container;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews().AddFluentValidation();
 
 // FluentValidation kurallarýný kaydedin
-builder.Services.AddTransient<IValidator<SendMessageDto>, SendContactValiator>();
-builder.Services.AddTransient<IValidator<Portfolio>, PortfolioValid>();
+builder.Services.ConfigureFluentValidation();
 
 builder.Services.AddDbContext<Context>();
 
 // Register services
-builder.Services.AddScoped<IAboutService, AboutManager>();
-builder.Services.AddScoped<IChooseusService, ChooseusManager>();
-builder.Services.AddScoped<IContactService, ContactManager>();
-builder.Services.AddScoped<IPortfolioService, PortfolioManager>();
-builder.Services.AddScoped<IPortfolioDetailsService, PortfolioDetailsManager>();
-builder.Services.AddScoped<IServicesService, ServicesManager>();
-builder.Services.AddScoped<ISkillsService, SkillsManager>();
-builder.Services.AddScoped<ISummaryService, SummaryManager>();
-builder.Services.AddScoped<IEducationService, EducationManager>();
-builder.Services.AddScoped<IExperienceService, ExperienceManager>();
-builder.Services.AddScoped<ITestimonialService, TestimonialManager>();
-builder.Services.AddScoped<IAppUserService, AppUserManager>();
-
+builder.Services.ConfigureRepositoryRegistration();
 // Register data access layers
-builder.Services.AddScoped<IAboutDal, EfAboutDal>();
-builder.Services.AddScoped<IChooseusDal, EfChooseusDal>();
-builder.Services.AddScoped<IContactDal, EfContactDal>();
-builder.Services.AddScoped<IPortfolioDal, EfPortfolioDal>();
-builder.Services.AddScoped<IPortfolioDetailsDal, EfPortfolioDetailsDal>();
-builder.Services.AddScoped<IServicesDal, EfServicesDal>();
-builder.Services.AddScoped<ISkillsDal, EfSkillsDal>();
-builder.Services.AddScoped<IExperienceDal, EfExperienceDal>();
-builder.Services.AddScoped<ISummaryDal, EfSummaryDal>();
-builder.Services.AddScoped<IEducationDal, EfEducationDal>();
-builder.Services.AddScoped<ITestimonialDal, EfTestimonialDal>();
-builder.Services.AddScoped<IAppUserDal, EfAppUserDal>();
+builder.Services.ConfigureServiceRegistration();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
-builder.Services.AddIdentity<AppUser, AppRole>(options =>
-{
-    options.Password.RequiredLength = 6;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireUppercase = true;
-})
-.AddEntityFrameworkStores<Context>()
-.AddErrorDescriber<CustomIdentityValidator>()
-.AddTokenProvider<DataProtectorTokenProvider<AppUser>>(TokenOptions.DefaultProvider);
+builder.Services.ConfigureIdentity();
 
-builder.Services.ConfigureApplicationCookie(options =>
-{
-    options.LoginPath = "/Account/SignIn/";
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
-    options.SlidingExpiration = true;
-});
+builder.Services.ConfigureApplicationCookie();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment()) 
+if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
